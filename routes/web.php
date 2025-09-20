595c235e4e86172f881b8ae4e2c840f6f87c57ac
syifaaawn <syifawahyuningsih25@gmail.com>
2025-09-20 10:43:56 +0700

    membuat login dengan breeze

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MahasiswaController;
-use App\Http\Controllers\MatakuliahController;
-use App\Http\Controllers\RuanganController;
 
-Route::get('/mahasiswa', [MahasiswaController::class, 'index']);
-Route::post('/mahasiswa', [MahasiswaController::class, 'store']);
+Route::get('/', function () {
+    return view('welcome');
+});
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/matakuliah', [MatakuliahController::class, 'index']);
-Route::post('/matakuliah', [MatakuliahController::class, 'store']);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::get('/ruangan', [RuanganController::class, 'index']);
-Route::post('/ruangan', [RuanganController::class, 'store']);
-
+require __DIR__.'/auth.php';
